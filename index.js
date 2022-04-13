ef368a7b2f2eb603cd040317eb349f74d8d5127e
felipehdev <felipehdev@gmail.com>
2022-04-13 16:19:17 -0300

    terminada a logica js. projeto pronto para entrega

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-function calendarBtn(params) {
-  let date = `&date=2014-10-01`;
-  let link = `https://api.nasa.gov/planetary/apod?api_key=4MIAo34S04lDB3jPTI39bq335LgZSfhJerDcgDJw${date}`;
+
+$("#submit").click(function (e) {
+  e.preventDefault();
+  loadContent();
+});
+
+function loadContent() {
+  const date = $("#calendar").val();
+  const img = $("#bgImg");
+  const title = $("#imgTitle");
+  const p = $("#img-p");
+  const video = $("#video");
+
+  const link = `https://api.nasa.gov/planetary/apod?api_key=4MIAo34S04lDB3jPTI39bq335LgZSfhJerDcgDJw&date=${date}`;
 
   $.ajax({
     type: "GET",
     url: link,
-    dataType: "dataType",
     success: function (response) {
-      console.log("Ready to launch 🚀");
+      if (response.media_type == "image") {
+        video.hide();
+        img.show();
+        img.attr("src", response.url);
+        title.text(response.title);
+        p.text(response.explanation);
+      } else {
+        img.hide();
+        video.show();
+        video.attr("src", response.url);
+        title.text(response.title);
+        p.text(response.explanation);
+      }
     },
   });
 }
-calendarBtn()
